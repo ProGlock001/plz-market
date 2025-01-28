aeee2b0f76643f0e98bb2c8c6344f2dd354d832d
Dmitry <babash.d@mail.ru>
2025-01-28 17:57:36 +0200

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,26 @@ const modal = document.querySelector(".search-modal");
 search.addEventListener("click", () => {
   modal.classList.toggle("modal_active");
 });
+
+$(function () {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../img/product/prev.png"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../img/product/next.png"></button>',
+    responsive: [
+      {
+        breakpoint: 545,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
